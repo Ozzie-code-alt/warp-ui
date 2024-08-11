7d2efa6ccb89344ff14ce2917ca9492478f070db
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-08-11 22:07:29 +0800

    refactor: changes in Button Types and Props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import './Button.css';
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
 }
 
-export default Button
+const Button: React.FC<ButtonProps> = ({ variant, ...props }) => {
+  const styles = () => {
+    const propStyle = props.style ?? {};
+    if (props.disabled) return propStyle;
+    if (variant === 'primary') return { borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA', ...propStyle };
+    if (variant === 'secondary') return { borderColor: '#6366F1', backgroundColor: '#FAFAFA', color: '#6366F1', ...propStyle };
+    return propStyle;
+  };
+
+  return (
+    <button {...props} style={styles()}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
